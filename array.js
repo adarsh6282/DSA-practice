761adfc976f69367a688d8e04d91a4a31df87793
Adarsh KP <kpadarsh41@gmail.com>
2025-01-02 21:00:11 +0530

    binary search

--- array.js
+++ array.js
@@ -143,3 +143,21 @@
 //     }
 // }
 
+//binary search
+
+function binarysearch(arr,target){
+    let left=0
+    let right=arr.length-1
+
+    while (left<=right) {
+        let mid=Math.floor((left+right)/2)
+
+        if(target===arr[mid]) return mid
+        if(target<arr[mid]){ right=mid-1}
+        else
+        { left=mid+1}
+    }
+    return -1
+}
+
+console.log(binarysearch([2,5,6,89,889],5))
