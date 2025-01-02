af0cbac9b01f263e26530ae2c3821a11b6b6bad3
Adarsh KP <kpadarsh41@gmail.com>
2025-01-02 10:18:06 +0530

    reverse an array

--- array.js
+++ array.js
@@ -1,17 +1,26 @@
 //find the largest and smallest element in an array
 
-let arr=[2,3,1,4,8]
+// let arr=[2,3,1,4,8]
 
-let small=arr[0]
-let large=0
+// let small=arr[0]
+// let large=0
 
-for(let i=0;i<arr.length;i++){
-    if(arr[i]<small){
-        small=arr[i]
-    }
-    else if(arr[i]>large)
-    {
-        large=arr[i]
-    }
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<small){
+//         small=arr[i]
+//     }
+//     else if(arr[i]>large)
+//     {
+//         large=arr[i]
+//     }
+// }
+// console.log(small,large)
+
+
+//reverse an array
+
+let arr=[2,3,6,8,4,1,9]
+
+for(let i=arr.length-1;i>=0;i--){
+    console.log(arr[i])
 }
-console.log(small,large)
